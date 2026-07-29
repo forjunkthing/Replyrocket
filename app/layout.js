dc5fdf2d0706886338438de9662d82b5ee7f04dd
forjunkthing <davefromtheparty@gmail.com>
2026-07-29 14:32:20 +0100

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'ReplyRocket | Review reply workspace',description:'Write thoughtful replies to customer reviews in seconds.'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
